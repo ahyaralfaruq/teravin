05db3b779d901bdf2491244e1cee66f824a61a6b
ahyaralfaruq <ahyaralfaruq97@gmail.com>
2022-08-29 21:17:51 +0700

    finally

--- src/Pages/Detail/Detail.jsx
+++ src/Pages/Detail/Detail.jsx
@@ -51,130 +51,111 @@ const Detail = ({ getPesertaBaru }) => {
 
    return (
       <Container className="container">
-         <Paper className="pre">
-            <Typography variant="h5" align="center" gutterBottom>
-               {initialData.id}
-            </Typography>
-            <Divider />
-            <div>
-               <Typography variant="h6" align="center" gutterBottom>
-                  Data diri
-               </Typography>
-               <Typography variant="body1">
-                  Nama : {initialData.nama}
-               </Typography>
-               <Typography variant="body1">
-                  Alamat : {initialData.alamat}
-               </Typography>
-               <Typography variant="body1">
-                  Tempat Lahir : {initialData.placeOfBirth}
-               </Typography>
-               <Typography variant="body1">
-                  Tanggal Lahir : {initialData.dateOfBirth}
-               </Typography>
-               <Typography variant="body1">
-                  Email : {initialData.email}
-               </Typography>
-               <Typography variant="body1">
-                  No. Hp : {initialData.nohp}
-               </Typography>
-               <Typography variant="body1">
-                  No. Rek : {initialData.norek}
-               </Typography>
-               <Typography variant="body1">
-                  Gender : {initialData.gender}
-               </Typography>
-               <Typography variant="body1">
-                  Agama : {initialData.agama}
-               </Typography>
-               <Typography variant="body1">
-                  Kewarganegaraan : {initialData.race}
-               </Typography>
-            </div>
-            <div>
-               <Typography variant="h6" align="center" gutterBottom>
-                  Riwayat Pendidikan
-               </Typography>
-               {initialData.riwayatPendidikan.map((rp) => (
-                  <div style={{ marginBottom: "30px" }}>
-                     <Typography variant="body1">
-                        Nama sekolah: {rp.namaSekolah}
+         {getPesertaBaru
+            .filter((pesertaBaru) => pesertaBaru.id === paramId.id)
+            .map((data) => (
+               <Paper className="pre">
+                  <Typography variant="h5" align="center" gutterBottom>
+                     {initialData.id}
+                  </Typography>
+                  <Divider />
+                  <div>
+                     <Typography variant="h6" align="center" gutterBottom>
+                        Data diri
                      </Typography>
                      <Typography variant="body1">
-                        Jurusan : {rp.jurusan}
-                     </Typography>
-                     <Typography variant="body1">Dari : {rp.dari}</Typography>
-                     <Typography variant="body1">
-                        Sampai : {rp.sampai}
+                        Nama : {initialData.nama}
                      </Typography>
                      <Typography variant="body1">
-                        Keterangan : {rp.keterangan}
+                        Alamat : {initialData.alamat}
+                     </Typography>
+                     <Typography variant="body1">
+                        Tempat Lahir : {initialData.placeOfBirth}
+                     </Typography>
+                     <Typography variant="body1">
+                        Tanggal Lahir : {initialData.dateOfBirth}
+                     </Typography>
+                     <Typography variant="body1">
+                        Email : {initialData.email}
+                     </Typography>
+                     <Typography variant="body1">
+                        No. Hp : {initialData.nohp}
+                     </Typography>
+                     <Typography variant="body1">
+                        No. Rek : {initialData.norek}
+                     </Typography>
+                     <Typography variant="body1">
+                        Gender : {initialData.gender}
+                     </Typography>
+                     <Typography variant="body1">
+                        Agama : {initialData.agama}
+                     </Typography>
+                     <Typography variant="body1">
+                        Kewarganegaraan : {initialData.race}
                      </Typography>
                   </div>
-               ))}
-            </div>
-            <div>
-               <Typography variant="h6" align="center" gutterBottom>
-                  Pengalaman Kerja
-               </Typography>
-               {initialData.pengalamanKerja.map((pk) => (
-                  <div style={{ marginBottom: "30px" }}>
-                     <Typography variant="body1">
-                        Nama perusahaan : {pk.namaPerusahaan}
+                  <div>
+                     <Typography variant="h6" align="center" gutterBottom>
+                        Riwayat Pendidikan
                      </Typography>
-                     <Typography variant="body1">
-                        Jenis industri : {pk.jenisIndustri}
-                     </Typography>
-                     <Typography variant="body1">Dari : {pk.dari}</Typography>
-                     <Typography variant="body1">
-                        Sampai : {pk.sampai}
-                     </Typography>
-                     <Typography variant="body1">
-                        Alasan keluar : {pk.alasan}
-                     </Typography>
+                     {initialData.riwayatPendidikan.map((rp) => (
+                        <div style={{ marginBottom: "30px" }}>
+                           <Typography variant="body1">
+                              Nama sekolah: {rp.namaSekolah}
+                           </Typography>
+                           <Typography variant="body1">
+                              Jurusan : {rp.jurusan}
+                           </Typography>
+                           <Typography variant="body1">
+                              Dari : {rp.dari}
+                           </Typography>
+                           <Typography variant="body1">
+                              Sampai : {rp.sampai}
+                           </Typography>
+                           <Typography variant="body1">
+                              Keterangan : {rp.keterangan}
+                           </Typography>
+                        </div>
+                     ))}
                   </div>
-               ))}
-            </div>
-            <div>
-               <Typography variant="h6" align="center" gutterBottom>
-                  Keahlian
-               </Typography>
-               {initialData.pengalamanKerja.map((k) => (
-                  <div style={{ marginBottom: "30px" }}>
-                     <Typography variant="body1">
-                        Keahlian : {k.namaPerusahaan}
+                  <div>
+                     <Typography variant="h6" align="center" gutterBottom>
+                        Pengalaman Kerja
                      </Typography>
+                     {initialData.pengalamanKerja.map((pk) => (
+                        <div style={{ marginBottom: "30px" }}>
+                           <Typography variant="body1">
+                              Nama perusahaan : {pk.namaPerusahaan}
+                           </Typography>
+                           <Typography variant="body1">
+                              Jenis industri : {pk.jenisIndustri}
+                           </Typography>
+                           <Typography variant="body1">
+                              Dari : {pk.dari}
+                           </Typography>
+                           <Typography variant="body1">
+                              Sampai : {pk.sampai}
+                           </Typography>
+                           <Typography variant="body1">
+                              Alasan keluar : {pk.alasan}
+                           </Typography>
+                        </div>
+                     ))}
                   </div>
-               ))}
-            </div>
-         </Paper>
-         {/* {paramId.id === 1 ? (
-            <Paper className="pre">
-               <Typography variant="h4" align="center" gutterBottom>
-                  {initialData.nama}
-               </Typography>
-               <Divider />
-               <Typography variant="body1">
-                  {JSON.stringify(initialData)}
-               </Typography>
-            </Paper>
-         ) : (
-            <>
-               {getPesertaBaru
-                  .filter((pesertaBaru) => pesertaBaru.id === paramId.id)
-                  .map((data) => (
-                     <Paper className="pre" key={data.id}>
-                        <Typography variant="h4" align="center" gutterBottom>
-                           {data.nama}
-                        </Typography>
-                        <Divider />
-                        <Typography variant="body1">
-                           {JSON.stringify(data)}
-                        </Typography>
-                     </Paper>
-                  ))}
-            </>
-         )} */}
+                  <div>
+                     <Typography variant="h6" align="center" gutterBottom>
+                        Keahlian
+                     </Typography>
+                     {initialData.pengalamanKerja.map((k) => (
+                        <div style={{ marginBottom: "30px" }}>
+                           <Typography variant="body1">
+                              Keahlian : {k.namaPerusahaan}
+                           </Typography>
+                        </div>
+                     ))}
+                  </div>
+               </Paper>
+            ))}
       </Container>
    );
 };
